7f2f9e1f9a34112efa393368c535c2dcdf7db519
Kotone Itaya <kotone@sfc.keio.ac.jp>
2018-02-16 17:44:58 +0900

    Restructure benchmark code

--- benchmark.cpp
+++ benchmark.cpp
@@ -41,72 +41,80 @@ class Load : public Functor {
   int delay;
 };
 
+void run(int procs, int delay, int payload, int n) {
+  int rank;
+  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+
+  std::vector<std::vector<Component*> > components(procs);
+  std::vector<Component*> flattened(n);
+
+  Load load(payload, delay);
+
+  for (int i = 0; i < n; ++i) {
+    Component* component = new Component(load, i % procs);
+    components[i % procs].push_back(component);
+    flattened[i] = component;
+  }
+
+  for (int i = 0; i < components.size(); ++i) {
+    for (int j = 0; j < components[i].size() - 1; ++j) {
+      int k = j + 1;
+      std::vector<Component*> inputs;
+      inputs.push_back(components[i][j]);
+      components[i][k]->connect(inputs);
+    }
+
+    if (i > 0) {
+      int j = i - 1;
+      std::vector<Component*> inputs;
+      inputs.push_back(components[i].front());
+      components[j].back()->connect(inputs);
+    }
+  }
+
+  MPI_Barrier(MPI_COMM_WORLD);
+
+  VTSScheduler s(flattened);
+
+  Timer timer;
+
+  int iters = 0;
+
+  while (iters < 10) {
+    s.step();
+    ++iters;
+  }
+
+  if (rank == 0) {
+    int elapsed = timer.elapsed();
+    std::cout << delay << " " << procs << " " << elapsed / iters << std::endl;
+  }
+
+  for (int i = 0; i < n; ++i) {
+    flattened[i]->wait();
+  }
+
+  for (std::size_t i = 0; i < flattened.size(); ++i) {
+    delete flattened[i];
+  }
+}
+
 int main(int argc, char* argv[]) {
-  int payload = 1000 * sizeof(float);
   int delay = 100;
+  int payload = 1000 * sizeof(float);
   int n = 1024;
 
   MPI_Init(&argc, &argv);
 
   int size;
-  int rank;
 
   MPI_Comm_size(MPI_COMM_WORLD, &size);
-  MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-  for (int procs = 1; procs <= size; ++procs) {
-    std::vector<std::vector<Component*> > components(procs);
-    std::vector<Component*> flattened(n);
-
-    Load load(payload, delay);
-
-    for (int i = 0; i < n; ++i) {
-      Component* component = new Component(load, i % procs);
-      components[i % procs].push_back(component);
-      flattened[i] = component;
+  for (int i = 0; i < 2; ++i) {
+    for (int procs = 1; procs <= size; ++procs) {
+      run(procs, delay, payload, n);
     }
-
-    for (int i = 0; i < components.size(); ++i) {
-      for (int j = 0; j < components[i].size() - 1; ++j) {
-        int k = j + 1;
-        std::vector<Component*> inputs;
-        inputs.push_back(components[i][j]);
-        components[i][k]->connect(inputs);
-      }
-
-      if (i > 0) {
-        int j = i - 1;
-        std::vector<Component*> inputs;
-        inputs.push_back(components[i].front());
-        components[j].back()->connect(inputs);
-      }
-    }
-
-    MPI_Barrier(MPI_COMM_WORLD);
-
-    VTSScheduler s(flattened);
-
-    Timer timer;
-
-    int iters = 0;
-
-    while (iters < 10) {
-      s.step();
-      ++iters;
-    }
-
-    if (rank == 0) {
-      int elapsed = timer.elapsed();
-      std::cout << procs << " " << elapsed / iters << std::endl;
-    }
-
-    for (int i = 0; i < n; ++i) {
-      flattened[i]->wait();
-    }
-
-    for (std::size_t i = 0; i < flattened.size(); ++i) {
-      delete flattened[i];
-    }
+    delay *= 10;
   }
 
   MPI_Finalize();
