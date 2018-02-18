9ae45408e31ad7ec609ed7a19be9fd9004b21372
ktnyt <kotone@sfc.keio.ac.jp>
2018-02-18 10:44:04 +0900

    Fix OpenMP pragmas

--- include/kbrica/scheduler.hpp
+++ include/kbrica/scheduler.hpp
@@ -18,22 +18,22 @@ class VTSScheduler {
   VTSScheduler(std::vector<Component*> components) : components_(components) {}
 
   void step() {
-#pragma omp for nowait
-    for (std::size_t i = 0; i < components_.size(); ++i) {
-      components_[i]->collect();
+#pragma omp parallel
+    {
+#pragma omp for
+      for (std::size_t i = 0; i < components_.size(); ++i) {
+        components_[i]->collect();
+      }
+#pragma omp for
+      for (std::size_t i = 0; i < components_.size(); ++i) {
+        components_[i]->execute();
+      }
+#pragma omp for
+      for (std::size_t i = 0; i < components_.size(); ++i) {
+        components_[i]->expose();
+      }
     }
 
-#pragma omp for nowait
-    for (std::size_t i = 0; i < components_.size(); ++i) {
-      components_[i]->execute();
-    }
-
-#pragma omp for nowait
-    for (std::size_t i = 0; i < components_.size(); ++i) {
-      components_[i]->expose();
-    }
-
-#pragma omp barrier
     MPI_Barrier(MPI_COMM_WORLD);
   }
 
